dc3be7e160b0316504d2535b2cd14b01e951a343
Santiago3110 <s.celisr2@uniandes.edu.co>
2024-05-30 14:09:54 -0500

    Arreglo

---
+++ manejador_documentos/manejador_documentos/mongo_client.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+
+def get_mongo_client():
+    client = MongoClient('mongodb://localhost:27017/')
+    return client
+def get_database(client, db_name):
+    return client[db_name]
