1e348ff92309b1006522301024ce72624e17c3c0
Maria Almeciga <malmeciga0104@gmail.com>
2024-05-30 12:29:01 -0500

    main app

--- main_project/main_project/views.py
+++ main_project/main_project/views.py
@@ -1,7 +1,19 @@
 import requests
 from django.http import JsonResponse
-"""
-def get_data_from_microservicio1(request):
-    response = requests.get('http://localhost:8001/api/simple/')
-    data = response.json()
-    return JsonResponse(data)"""
+
+def orquestador_view(request):
+    if request.method == 'POST':
+        # Procesar la solicitud del usuario
+        data_from_user = request.POST.get('data')
+        
+        # Enviar la solicitud al microservicio
+        response = requests.post('http://localhost:8001/api/orquestador/', json=data_from_user)
+
+        # Analizar y devolver la respuesta del microservicio al usuario
+        if response.status_code == 200:
+            data_from_microservicio = response.json()
+            return JsonResponse(data_from_microservicio)
+        else:
+            return JsonResponse({'error': 'Could not communicate with microservice'}, status=500)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
